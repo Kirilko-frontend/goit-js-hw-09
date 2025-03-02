1ec9c9f463d44895c2f62d6c38c34b27b671f5ca
Kirilko-frontend <kkir614@gmail.com>
2025-03-02 11:22:01 +0100

    update build

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,17 +1,12 @@
-// 2-form.js
-
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
-// Объект formData
 let formData = { email: '', message: '' };
 
-// Функция для обновления formData и записи в localStorage
 function updateStorage() {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-// Проверяем наличие данных в localStorage при загрузке страницы
 const savedData = localStorage.getItem(STORAGE_KEY);
 if (savedData) {
   formData = JSON.parse(savedData);
@@ -19,13 +14,11 @@ if (savedData) {
   form.message.value = formData.message || '';
 }
 
-// Обработчик события input
 form.addEventListener('input', event => {
   formData[event.target.name] = event.target.value.trim();
   updateStorage();
 });
 
-// Обработчик события submit
 form.addEventListener('submit', event => {
   event.preventDefault();
 
